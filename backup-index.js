e1e94b1668533a846bc7e4d06d07478942a17969
C0nsp1racy <74108866+C0nsp1racy@users.noreply.github.com>
2021-03-29 19:59:06 +0200

    Update backup-index.js

--- backup-index.js
+++ backup-index.js
@@ -1,39 +1,53 @@
-const express = require('express')
-const app = express()
-app.listen(3000)
-const { search } = require("./api/index.js")
-
-app.all("/download", async (req, res) => {
-    if(req.query.titleID){
-      await search(req.query.titleID, res, req)
-    } else {
-      res.sendStatus(403)
+const fs = require('fs'), { google } = require('googleapis'),drive = google.drive({ version: 'v3' });
+var SAarray = require("./SAs.json"), SAIndex = 0, res, access_token, files, https = require('https');
+const contentDisposition = require('content-disposition');
+module.exports = {
+    async search(titleID, response, request) {
+        let serviceAccount = new google.auth.JWT(SAarray[SAIndex].client_email, null, SAarray[SAIndex].private_key, ['https://www.googleapis.com/auth/drive'])
+        serviceAccount.authorize(async (err, tokens) => {
+            if (err) return SAIndex++
+        });
+        drive.files.list({
+            "auth": serviceAccount,
+            "corpora": "allDrives",
+            "pageSize": 400,
+            "supportsAllDrives": true,
+            "includeItemsFromAllDrives": true,
+            "q": `name contains "${titleID}"`
+        }).then(async resp => {
+            files = resp.data.files
+            let options = {
+                "supportsAllDrives": true,
+                "supportsTeamDrives": true
+            }
+            if(files.length == 0) return response.sendStatus(404)
+            for (let i = 0; i < files.length; i++) {
+                try {
+                    serviceAccount = new google.auth.JWT(SAarray[i].client_email, null, SAarray[i].private_key, ['https://www.googleapis.com/auth/drive'])
+                    serviceAccount.authorize(async (err, tokens) => {
+                        access_token = tokens.access_token
+                    })
+                    options.auth = serviceAccount
+                    options.fileId = files[i].id
+                    res = await drive.files.copy(options)
+                    i = files.length
+                } catch (e) {
+                    console.log(e);
+                }
+            }
+            response.header('Content-Disposition', contentDisposition(res.data.name));
+            options = {
+                host: "www.googleapis.com",
+                path: `/drive/v3/files/${res.data.id}?alt=media`,
+                headers: { 'Authorization': `Bearer ${access_token}` }
+            }
+            https.get(options, (remote_resp) => {
+              remote_resp.pipe(response)
+            }).on('error', (e) => {
+             console.error(e);
+            });
+        }).catch(e => {
+            console.log(e)
+        })
     }
-})
-
-app.get('/mirror', (req, res) => {
-	res.download(__dirname + '/mirror.tfl');
-});
-
-app.get('*', (req, res) => {
-	res.sendFile(__dirname + '/index.html');
-});
-
-const { exec } = require('child_process');
-let i = 0;
-
-function start() {
-	exec(`bash index.sh`, (error, stdout, stderr) => {
-		if (stderr) console.log(stderr);
-		console.log(stdout);
-		setTimeout(start, 60 * 60 * 1000 * 2);
-	});
 }
-
-start();
-
-app.get('/startindex', async (req, res) => {
-  await require("axios").get("repl 1 URL address/recieveCommands", { 
-      headers: { "commandToBeExecuted": "echo Hello from repl 2", "auth": "43274%@#(FFAS_!!" }
-   })
-});
